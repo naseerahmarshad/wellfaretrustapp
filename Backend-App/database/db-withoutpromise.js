58868de5bd6762ab84c5273e38115ac01b414a58
naseerahmarshad <designverx@gmail.com>
2024-08-03 22:02:16 +0530

    update 2

--- Backend-App/database/db-withoutpromise.js
+++ Backend-App/database/db-withoutpromise.js
@@ -3,10 +3,6 @@ import { config as dotenvConfig } from 'dotenv';
 dotenvConfig();
 
 const dbwithoutpromise = mysql.createPool({
-    // host: 'localhost',
-    // user: 'root',
-    // password: 'mysql',
-    // database: 'nodetest'
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
